7c3713228371ed1c8915ebc8a819cf1fc87f0806
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-01 23:18:22 -0300

    feat: opções da primeira parte

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function rodar_imagem1(){
-    var imagem_adc = window.document.getElementById('ad_resposta')
+    var imagem_adc = window.document.getElementById('adc_resposta')
+    var adc_opcoes = window.document.getElementsByName('adc')
+    var adc_escolhido = ''
+    if (adc_opcoes[0].checked){
+        var adc_escolhido = 'tristana'
+    } else if (adc_opcoes[1].checked){
+        var adc_escolhido = 'ashe'
+    } else if (adc_opcoes[2].checked){
+        var adc_escolhido = 'sivir'
+    }
+
+    adc_resposta.innerhtml = `${adc_escolhido}`
 }
