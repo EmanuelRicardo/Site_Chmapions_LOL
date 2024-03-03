a81d88b633a6033441812255145e19fa1d1cdfb2
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-03 19:01:34 -0300

    feat: Imagem de background + img aparecendo

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+//AD carry
 function rodar_imagem1(){
     var imagem_adc = window.document.getElementById('adc_resposta')
     var adc_opcoes = window.document.getElementsByName('adc')
     var adc_escolhido = ''
+    var img = document.createElement('img')
+
+    img.setAttribute('id', 'foto')
+    
+    imagem_adc.innerHTML = ' '
     if (adc_opcoes[0].checked){
-        var adc_escolhido = 'Tristana'
+        img.setAttribute ('src', 'imagens/tristana.png')
     } else if (adc_opcoes[1].checked){
+        img.setAttribute ('src', 'imagens/ashe.png')
         var adc_escolhido = 'Ashe'
     } else if (adc_opcoes[2].checked){
         var adc_escolhido = 'Sivir'
+        img.setAttribute ('src', 'imagens/sivir.png')
     }
+
+    imagem_adc.appendChild(img)
+}
+
+// MID
+
+function rodar_imagem2(){
+    
 }
